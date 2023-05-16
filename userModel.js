f1e1a51a7ce6dd80c9964b98bd5ebd87085def6e
domieee <gartzdominik@gmail.com>
2023-05-16 14:23:55 +0200

    update: added dynamic id

--- userModel.js
+++ userModel.js
@@ -1,61 +1,59 @@
 import mongoose from 'mongoose'
 
 const commentUnderComment = mongoose.Schema({
-    profileImage:String,
-    userName:String,
-    jobTitle:String,
-    commentText:String
+    profileImage: String,
+    userName: String,
+    jobTitle: String,
+    commentText: String
 })
 
 const commentSchema = mongoose.Schema({
-    profileImage:String,
-    userName:String,
-    jobTitle:String,
-    userId:String,
-    commentText:String,
-    likes:Number,
-    commentIdTest:String,
-    comments:[commentUnderComment]
+    profileImage: String,
+    userName: String,
+    jobTitle: String,
+    userId: String,
+    commentText: String,
+    likes: Number,
+    commentIdTest: String,
+    comments: [commentUnderComment]
 })
 
 const postSchema = mongoose.Schema({
-    postId:String,
-    profileImage:String,
-    userName:String,
-    jobTitle:String,
-    postDescription:String,
-    postImage:String,
-    likes:Number,
-    userId:String,
-    comments:[commentSchema]
-    
+    postId: String,
+    profileImage: String,
+    userName: String,
+    jobTitle: String,
+    postDescription: String,
+    postImage: String,
+    likes: Number,
+    userId: String,
+    comments: [commentSchema]
 }
 )
 
 const follower = mongoose.Schema({
-    followerName:String,
-    jobTitle:String,
-    avatarSmall:String
+    followerName: String,
+    jobTitle: String,
+    avatarSmall: String
 })
 
 const userSchema = mongoose.Schema({
-    mail:String,
-    password:String,
-    userName:String,
+    mail: String,
+    password: String,
+    fullName: String,
+    firstName: String,
+    lastName: String,
+    birthDate: String,
+    telephoneNumber: String,
+    gender: String,
+    profileDescription: String,
+    profileWebsite: String,
+    avatarSmall: String,
+    jobTitle: String,
+    posts: [postSchema],
 
-    firstName:String,
-    lastName:String,
-    birthDate:String,
-    telephoneNumber:String,
-    gender:String,
-    profileDescription:String,
-    profileWebsite:String,
-    profileImage:String,
-    jobTitle:String,
-    posts:[postSchema],
-
-    followerList:[follower],
-    followingList:[follower]
+    followerList: [follower],
+    followingList: [follower]
 })
 
 
@@ -64,6 +62,6 @@ const userSchema = mongoose.Schema({
 
 
 
-const userData = mongoose.model('user',userSchema )
+const userData = mongoose.model('user', userSchema)
 
 export default userData
